b6a3fdc7a51d8ddd24a00afa48c3fb39dfae771e
Ibrahim <imorketa9@gmail.com>
2024-01-08 13:49:19 +0000

    fix pycodestyle

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-    return (l, sentence[0] if l > 0 else None)
+    length = len(sentence)
+    return (length, sentence[0] if length > 0 else None)
