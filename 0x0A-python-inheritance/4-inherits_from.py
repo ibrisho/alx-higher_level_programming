0c5006a67cbf693b47c2044e6e0cc10ff0d6ce2d
Ibrahim <imorketa9@gmail.com>
2024-02-07 11:20:34 +0000

    fix pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns True if object is subclass of input class"""
-    return (isinstance(obj, a_class) and (type(obj) != a_class))
+    return (isinstance(obj, a_class) and (type(obj) is not a_class))
